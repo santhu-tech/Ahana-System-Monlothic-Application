d63e131fcda60e646359aec087c34493da0302d6
santhu-tech <psanthiyamahe@gmail.com>
2024-11-04 15:07:18 +0530

    Add files via upload

---
+++ Day-3-Microservices/CloudGatway/CloudGatway/src/main/java/com/example/CloudGatway/CloudGatwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.CloudGatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatwayApplication.class, args);
+	}
+
+}
